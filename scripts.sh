0817bccfeb0d2f795e184c342668ead04fdde614
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-06-23 10:12:04 +1200

    setting up project

---
+++ scripts.sh
@@ -0,0 +1,7 @@
+# npx create-expo-app my-app
+# yarn add nativewind
+# yarn add --dev tailwindcss
+# https://reactnavigation.org/docs/getting-started/ for the below docs
+# npm install @react-navigation/native
+# npx expo install react-native-screens react-native-safe-area-context
+# npx prettier --write .
