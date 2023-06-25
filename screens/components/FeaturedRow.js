79fabfa25dd486848c69f2da8a82766aedfffdca
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-06-26 11:30:31 +1200

    restaurant cards info pulled from sanity and displayed on front

--- screens/components/FeaturedRow.js
+++ screens/components/FeaturedRow.js
@@ -1,12 +1,34 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ArrowRightIcon } from "react-native-heroicons/outline";
 import RestaurantCard from "./RestaurantCard";
+import sanityClient from "../../sanity";
 
 const img =
   "https://plus.unsplash.com/premium_photo-1667545168921-34f756495d7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1746&q=80";
 
 export default function FeaturedRow({ id, title, description }) {
+  const [restaurants, setRestaurants] = useState([]);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "featured" && _id == $id] {
+      ...,
+      restaurants[]->{
+        ...,
+        dishes[]->,
+        type-> {
+          name
+        }
+      },
+    }[0]`,
+        { id }
+      )
+      .then((data) => {
+        setRestaurants(data?.restaurants);
+      });
+  }, []);
+
   return (
     <View>
       <View className="mt-4 flex-row items-center justify-between px-4">
@@ -22,42 +44,21 @@ export default function FeaturedRow({ id, title, description }) {
           paddingHorizontal: 15,
         }}
       >
-        <RestaurantCard
-          id={id}
-          imgUrl={img}
-          title="Yo! Sushi"
-          rating={(4, 5)}
-          genre="Japanese"
-          address="123, Fake Street, London"
-          short_description="Sushi, Japanese, Asian, Healthy, Vegetarian Friendly, Vegan Options, Gluten Free Options"
-          dishes={[]}
-          long={-0.1277583}
-          lat={51.5073509}
-        />
-        <RestaurantCard
-          id={id}
-          imgUrl={img}
-          title="Yo! Sushi"
-          rating={(4, 5)}
-          genre="Japanese"
-          address="123, Fake Street, London"
-          short_description="Sushi, Japanese, Asian, Healthy, Vegetarian Friendly, Vegan Options, Gluten Free Options"
-          dishes={[]}
-          long={-0.1277583}
-          lat={51.5073509}
-        />
-        <RestaurantCard
-          id={id}
-          imgUrl={img}
-          title="Yo! Sushi"
-          rating={(4, 5)}
-          genre="Japanese"
-          address="123, Fake Street, London"
-          short_description="Sushi, Japanese, Asian, Healthy, Vegetarian Friendly, Vegan Options, Gluten Free Options"
-          dishes={[]}
-          long={-0.1277583}
-          lat={51.5073509}
-        />
+        {restaurants?.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant._id}
+            id={restaurant._id}
+            imgUrl={restaurant.image}
+            address={restaurant.address}
+            title={restaurant.name}
+            dishes={restaurant.dishes}
+            rating={restaurant.rating}
+            short_description={restaurant.short_description}
+            genre={restaurant.type?.name}
+            long={restaurant.long}
+            lat={restaurant.lat}
+          />
+        ))}
       </ScrollView>
     </View>
   );
