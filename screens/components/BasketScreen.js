c66c7797f8557b607428b4ed8ebd3022d293b880
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-06-27 10:12:14 +1200

    fixed bugs and finished basket screen

--- screens/components/BasketScreen.js
+++ screens/components/BasketScreen.js
@@ -10,26 +10,32 @@ import React, { useMemo, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useDispatch, useSelector } from "react-redux";
 import { selectRestaurant } from "../../features/restaurantSlice";
-import { selectBasketItems } from "../../features/basketSlice";
+import {
+  removeFromBasket,
+  selectBasketItems,
+  selectBasketTotal,
+} from "../../features/basketSlice";
 import { XCircleIcon } from "react-native-heroicons/solid";
 import { urlFor } from "../../sanity";
-
-const img =
-  "https://plus.unsplash.com/premium_photo-1667545168921-34f756495d7b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1746&q=80";
+import Currency from "react-currency-formatter";
 
 export default function BasketScreen() {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
   const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBasketTotal);
   const dispatch = useDispatch();
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
 
   useMemo(() => {
     const groupedItems = items.reduce((results, item) => {
       (results[item.id] = results[item.id] || []).push(item);
-    });
+      return results;
+    }, {});
     setGroupedItemsInBasket(groupedItems);
   }, [items]);
+
+  console.log(groupedItemsInBasket);
 
   return (
     <SafeAreaView className="flex-1 bg-white">
@@ -52,7 +58,7 @@ export default function BasketScreen() {
         <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
           <Image
             source={{
-              uri: { img },
+              uri: "https://images.unsplash.com/photo-1610478920409-ec0f58e881a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
@@ -61,25 +67,65 @@ export default function BasketScreen() {
             <Text className="text-[#00ccbb]">Change</Text>
           </TouchableOpacity>
         </View>
-        <View>
-          <ScrollView>
-            {Object.entries(groupedItemsInBasket).map(([key, items]) => (
-              <View key={key}>
-                <Text>{items.length} x</Text>
-                {items[0]?.image ? (
-                  <Image
-                    source={{
-                      uri: urlFor(items[0]?.image).url(),
-                    }}
-                    className="h-12 w-12 rounded-full"
-                  />
-                ) : (
-                  <Text>No image</Text>
-                )}
-                <Text className="flex-1">{items[0]?.name}</Text>
-              </View>
-            ))}
-          </ScrollView>
+        <ScrollView className="divide-y divide-gray-200">
+          {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+            <View
+              key={key}
+              className="flex-row items-center space-x-3 bg-white py-2 px-5"
+            >
+              <Text className="text-[#00CCBB]">{items.length} x</Text>
+              {items[0]?.image ? (
+                <Image
+                  source={{
+                    uri: urlFor(items[0]?.image).url(),
+                  }}
+                  className="h-12 w-12 rounded-full"
+                />
+              ) : (
+                <Text>No image</Text>
+              )}
+              <Text className="flex-1">{items[0]?.name}</Text>
+
+              <Text className="text-gray-600">
+                <Currency quantity={items[0]?.price} currency="NZD" />
+              </Text>
+
+              <TouchableOpacity>
+                <Text
+                  className="text-[#00ccbb] text-xs"
+                  onPress={() => dispatch(removeFromBasket({ id: key }))}
+                >
+                  Remove
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View className="p-5 bg-white mt-5 space-y-4">
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Subtotal</Text>
+            <Text className="text-gray-400">
+              <Currency quantity={basketTotal} currency="NZD" />
+            </Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text className="text-gray-400">Delivery Fee</Text>
+            <Text className="text-gray-400">
+              <Currency quantity={5.99} currency="NZD" />
+            </Text>
+          </View>
+          <View className="flex-row justify-between">
+            <Text>Order Total</Text>
+            <Text className="font-extrabold">
+              <Currency quantity={basketTotal + 5.99} currency="NZD" />
+            </Text>
+          </View>
+          <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4">
+            <Text className="text-center text-white text-lg font-bold">
+              Place Order
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
