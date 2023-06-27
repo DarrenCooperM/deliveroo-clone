09076a6dbbc42f1e4fef65411ea7e1d05e1bfab3
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-06-27 12:50:18 +1200

    added notes to scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -5,3 +5,14 @@
 # npm install @react-navigation/native
 # npx expo install react-native-screens react-native-safe-area-context
 # npx prettier --write .
+# sanity start - start backend and access to UI
+
+# OTHER NOTES
+# expo install react-native-maps - MAP integration
+# yarn add react-native-animatable --save - ANIMATIONS
+# yarn add react-native-progress --save - PROGRESS BAR
+
+# REDUX
+# yard add @reduxjs/toolkit
+# yarn add react-redux
+# https://redux-toolkit.js.org/introduction/getting-started
